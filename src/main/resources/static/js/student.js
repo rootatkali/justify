cbb558c914379084fc20873ffafb1986dc479e65
Rotem Moses <rotem@faceco.de>
2021-03-23 20:15:40 +0200

    Add student edit request code

--- src/main/resources/static/js/student.js
+++ src/main/resources/static/js/student.js
@@ -35,9 +35,18 @@ function loadRequests(requests) {
     let justificationCode = r.justificationCode;
     let eventName = events.filter(e => e.code === eventCode)[0].name;
     let jstfnName = justifications.filter(j => j.code === justificationCode)[0].name;
-    let cancel = r.status === "UNANSWERED" ?
-      ` <a href="#" onclick="cancelRequest(${r.requestId})"><i class="far fa-trash-alt" style="color: red"></i></a>`
-      : "";
+    let status = r.status === "UNANSWERED" ?
+      `
+<div class="btn-group btn-group-sm" role="group" aria-label="Action buttons for request">
+  <button type="button" class="btn btn-danger" onclick="cancelRequest(${r.requestId})">
+    <i class="fal fa-trash-alt" style="color: #fff"></i>
+  </button>
+  <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#editModal" data-bs-id="${r.requestId}">
+    <i class="fal fa-edit" style="color: #fff"></i>
+  </button>
+</div>
+`
+      : r.status;
     let tr = `<tr ${color}>
 <td>${r.requestId}</td>
 <td>${r.dateStart}</td>
@@ -47,7 +56,7 @@ function loadRequests(requests) {
 <td>${eventName}</td>
 <td>${jstfnName}</td>
 <td>${r.note}</td>
-<td>${r.status}${cancel}</td>
+<td>${status}</td>
 </tr>`
     tb.innerHTML += tr;
   });
@@ -72,6 +81,7 @@ function loadEvents() {
   events.forEach(e => {
     select.innerHTML += `<option value="${e.code}">${e.name}</option>`;
   });
+  document.getElementById("edit-event").innerHTML = select.innerHTML;
 }
 
 function loadApprovals() {
@@ -80,6 +90,7 @@ function loadApprovals() {
   justifications.forEach(j => {
     select.innerHTML += `<option value="${j.code}">${j.name}</option>`;
   });
+  document.getElementById("edit-approval").innerHTML = select.innerHTML;
 }
 
 function sendRequest() {
@@ -129,4 +140,42 @@ document.getElementById("btnLogout").onclick = function () {
 let mashovId = Cookies.get("mashovId");
 $.get(`/api/users/${mashovId}`, data => {
   $("#name").text(data.firstName + " " + data.lastName);
+});
+
+const editModal = document.getElementById('editModal');
+editModal.addEventListener('show.bs.modal', event => {
+  const btn = event.relatedTarget;
+  const id = btn.getAttribute('data-bs-id');
+  $.get(`/api/requests/${id}`, data => {
+    $('#editModalTitle').text(`Editing request ${id}`);
+    document.getElementById('edit-date-start').value = data.dateStart;
+    document.getElementById('edit-period-start').value = data.periodStart;
+    document.getElementById('edit-date-end').value = data.dateEnd;
+    document.getElementById('edit-period-end').value = data.periodEnd;
+    document.getElementById('edit-event').value = data.eventCode;
+    document.getElementById('edit-approval').value = data.justificationCode;
+    document.getElementById('edit-note').value = data.note;
+    document.getElementById('edit-sendRequest').onclick = () => {
+      $.ajax(`/api/requests/${id}`, {
+        method: 'PUT',
+        contentType: 'application/json',
+        data: JSON.stringify({
+          mashovId: data.mashovId,
+          dateStart: $('#edit-date-start').val(),
+          dateEnd: $('#edit-date-end').val(),
+          periodStart: parseInt($('#edit-period-start').val()),
+          periodEnd: parseInt($('#edit-period-end').val()),
+          eventCode: parseInt($('#edit-event').val()),
+          justificationCode: parseInt($('#edit-approval').val()),
+          note: $('#edit-note').val()
+        }),
+        success: successData => {
+          location.reload();
+        },
+        error: (jqXHR, textStatus, errorThrown) => {
+          alert(errorThrown)
+        }
+      });
+    };
+  });
 });
